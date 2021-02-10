5aaf22b46b2de9936f0f5bae696ce46ab49de542
dxglide <dxglide@gmail.com>
2021-02-10 21:37:28 +0200

    init

---
+++ src/main/java/org/tomostuff/springboot/tests/swaggergen/restswaggeropenapitest/RestSwaggerOpenapiTestApplication.java
@@ -0,0 +1,13 @@
+package org.tomostuff.springboot.tests.swaggergen.restswaggeropenapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSwaggerOpenapiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSwaggerOpenapiTestApplication.class, args);
+	}
+
+}
